f3012f30785019e3ce77b5203f4d6ca90c380009
yhb <johnddoe@yuu.com>
2019-06-03 21:47:03 +0800

    test

--- src/main/java/com/yy/yycustom/CustomController/CustomController.java
+++ src/main/java/com/yy/yycustom/CustomController/CustomController.java
@@ -54,8 +54,6 @@ public class CustomController {
 
         UserBean  user  = new UserBean();
 
-
-
         user.setUid("yy");
         user.setUname("11111查无此人");
         user.setUphone("号码为空11111");
